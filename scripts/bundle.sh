2a18625fe3fd271273e5863935f16a2c440b9a7c
dotfold <jamesmcnamee@gmail.com>
2017-05-15 13:38:49 +1000

    [build] bundle the correct directory

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -5,4 +5,4 @@ GIT_SHA=$(git rev-parse --short HEAD)
 TAR_TARGET="deploy-$GIT_SHA.tar.gz"
 
 mkdir -p deploy
-tar -czvf deploy/deploy-$GIT_SHA.tar.gz build/*
+tar -czvf deploy/deploy-$GIT_SHA.tar.gz dist/*

aa3b700482d9550e5ff60ffddbb010300ee519cb
dotfold <jamesmcnamee@gmail.com>
2017-05-15 11:50:56 +1000

    [build] move docker login and push inside the image build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,5 @@ docker build -t react-ssr-docker .
 # docker tag -f ${FULL_IMAGE_NAME}:${TAG_NAME} ${FULL_IMAGE_NAME}:latest
 # docker tag 396984747102.dkr.ecr.us-west-2.amazonaws.com/react-ssr-docker:${GIT_SHA} 396984747102.dkr.ecr.us-west-2.amazonaws.com/react-ssr-docker:latest
 docker tag react-ssr-docker:latest 396984747102.dkr.ecr.us-west-2.amazonaws.com/react-ssr-docker:latest
+docker login -u "AWS" -p "$AWS_DOCKER_PASSWORD" -e none https://396984747102.dkr.ecr.us-west-2.amazonaws.com
+docker push 396984747102.dkr.ecr.us-west-2.amazonaws.com/react-ssr-docker:latest

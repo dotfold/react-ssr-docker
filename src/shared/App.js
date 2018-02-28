14bac53bb93f16a5956978e1968835b46b33008d
dotfold <james@dotfold.io>
2018-02-27 21:50:26 -0800

    feat(app): redirect and add other routes

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,18 +1,60 @@
 import React from 'react'
 import Main from './Main'
 import Home from './Home'
-import {Link, Route} from 'react-router-dom'
+import {Link, Route, Redirect} from 'react-router-dom'
 
 const style = {
   display: 'flex',
   alignItems: 'stretch'
 }
 
-export default () => (
-  <div style={style}>
-    <Main>
-      <Route path="/" exact component={Home} />
-    </Main>
-  </div>
+const RedirectWithStatus = ({ from, to, status }) => (
+  <Route render={({ staticContext }) => {
+    // there is no `staticContext` on the client, so
+    // we need to guard against that here
+    if (staticContext)
+      staticContext.status = status
+    return <Redirect from={from} to={to}/>
+  }}/>
 )
+
+const Another = () => <div>another route</div>
+const Layout = ({component: Component, ...rest}: LayoutComponentProps) => {
+  return (
+    <Route {...rest} render={matchProps => (
+      <div>
+        <Component {...matchProps} />
+      </div>
+    )} />
+  )
+}
+
+export default class extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {redirectToSignup: false}
+  }
+
+  navigate = () => {
+    this.setState({redirectToSignup: true})
+  }
+
+  render () {
+    if (this.state.redirectToSignup) return <Redirect push={true} to="/signup" />
+    return (
+      <div style={style}>
+        <Main>
+          <Route path="/" exact component={Home} />
+          <Layout path='/another' component={Another} />
+        </Main>
+        <Link to="/another">another</Link>
+        <Link to="/signup">link to signup</Link>
+        <a href="/signup">href to signup</a>
+        <button onClick={this.navigate}>SIGNUP PAGE</button>
+      </div>
+    )
+  }
+}
+
+// <RedirectWithStatus from="/" to="/signup" status={302} />
 
